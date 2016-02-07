3a2cd4c7c764114a80be724a339153a4c20d326b
Nicholas Hanson-Holtry <nicholas.hanson.holtry@gmail.com>
2016-02-07 14:51:15 -0600

    does m*n matches and proper printing

--- match.py
+++ match.py
@@ -1,5 +1,5 @@
 
-import csv
+import sys, csv
 
 def match ():
 
@@ -7,6 +7,9 @@ def match ():
             open('format.txt', 'r') as format_file:
 
         ### STEP 1: Parse the format file. ###
+
+        print("Step 1")
+        sys.stdout.flush()
 
         index_of_ = {}
         meaning_of_ = {}
@@ -16,6 +19,10 @@ def match ():
             row = row.strip()
 
             if row == "": continue
+
+            format_entry = row.split(":")
+            key = format_entry[0]
+            value = format_entry[1]
 
             def char_to_index (char_array):
                 # create the array of indices to output
@@ -45,10 +52,6 @@ def match ():
             valid_keys_multiple = ["matches1", "matches2", "info", "questions"]
             valid_keys = valid_keys_one + valid_keys_multiple
 
-            format_entry = row.split(":")
-            key = format_entry[0]
-            value = format_entry[1]
-
             if key in valid_keys:
 
                 key = key
@@ -66,21 +69,26 @@ def match ():
 
                 meaning_of_[tuple(char_to_index(key))] = value
 
-        new_matches2_index = []
-        for index in index_of_["matches2"]:
+        index_of_overall = tuple([-1])
+        meaning_of_[index_of_overall] = "Overall"
+
+        def newtuple (index):
             if type(index) is list:
                 index = tuple(index)
             elif type(index) is int:
                 index = tuple([index])
+            return index
+
+        new_matches2_index = []
+        for index in index_of_["matches2"]:
+            index = newtuple(index)
             new_matches2_index.append(index)
         index_of_["matches2"] = new_matches2_index
 
-        print("index_of_")
-        print(index_of_)
-        print("meaning_of_")
-        print(meaning_of_)
-
         ### STEP 2: Parse the data file. ###
+        
+        print("Step 2")
+        sys.stdout.flush()
 
         people = {}
 
@@ -89,7 +97,7 @@ def match ():
         constant = 0
 
         i = 0
-        expected_num_answers = 0;
+        expected_num_answers = 0
         for row in csv.reader(data_file):
             i += 1
             if i == 1:
@@ -111,10 +119,10 @@ def match ():
 
             people[answers[index_of_["name"]]] = tuple(answers)
 
-        print("people")
-        print(people)
-
         ### STEP 3: Compute similarity scores. ###
+        
+        print("Step 3")
+        sys.stdout.flush()
 
         similarity_scores = {}
 
@@ -136,8 +144,12 @@ def match ():
                     similarity_scores[names2] = score
 
         ### STEP 4: Compute compatible matches. ###
+        
+        print("Step 4")
+        sys.stdout.flush()
 
         matches = {}
+        emptyID = string2constant[""]
 
         for person1id in people:
             person1 = people[person1id]
@@ -162,7 +174,7 @@ def match ():
                                 new_matches_list.append((person2id, score))
 
                 new_matches_results[index1] = {}
-                new_matches_results[index1][-1] = new_matches_list
+                new_matches_results[index1][index_of_overall] = new_matches_list
 
                 all_matches = new_matches_list
                 for index2 in index_of_["matches2"]:
@@ -179,9 +191,11 @@ def match ():
 
                         compatible = False
                         for value1 in persion1values:
-                            for value2 in persion2values:
-                                if value1 == value2:
-                                    compatible = True
+                            if value1 != emptyID:
+                                for value2 in persion2values:
+                                    if value2 != emptyID:
+                                        if value1 == value2:
+                                            compatible = True
 
                         if compatible:
                             new_matches_list.append(pair)
@@ -191,6 +205,11 @@ def match ():
             matches[person1id] = new_matches_results
 
         ### STEP 5: Compute best matches. ###
+        
+        print("Step 5")
+        sys.stdout.flush()
+
+        index_of_["matches2"] = [index_of_overall] + index_of_["matches2"]
 
         for person1id in matches:
             for index1 in index_of_["matches1"]:
@@ -199,108 +218,127 @@ def match ():
                             matches[person1id][index1][index2],
                             key = lambda pair: -pair[1])[:10]
 
-        print("matches")
-        print(matches)
-
         ### STEP 6: Output best matches. ###
+        
+        print("Step 6")
+        sys.stdout.flush()
 
         num_questions = len(index_of_["questions"])
 
-        # for person1id in matches:
-        #     print("matches for " + constant2string[person1id] + ":")
-        #     for match_index in index_of_["matches1"]:
-        #         print("    " + meaning_of_[match_index] + " matches:")
-        #         for match in matches[person1id][match_index]:
-        #             print("        " + constant2string[match[0]] + \
-        #                     " (" + str(match[1] / num_questions) + ")")
+        i = 0
+        for person1id in people:
+            person1 = people[person1id]
 
-#         def output_one_page (person, matches, i):
+            netID = constant2string[person1[index_of_["email"]]].split("@")[0].lower()
+            with open('./results/'+netID+'_'+str(i)+'.tex', 'w', encoding='utf-8') as output_file:
+                i += 1
 
-#             netID = person[index_of_["email"]].split("@")[0]
-#             with open('./results/'+netID+'.'+i+'.tex', 'w') as output_file:
+                output_file.write("\n")
+                output_file.write("\\documentclass[a4paper]{article}\n")
+                output_file.write("\n")
+                output_file.write("\\usepackage[english]{babel}\n")
+                output_file.write("\\usepackage[utf8]{inputenc}\n")
+                output_file.write("\\usepackage{parskip}\n")
+                output_file.write("\n")
+                output_file.write("\\usepackage{geometry}\n")
+                output_file.write("\\geometry{\n")
+                output_file.write("    tmargin   = 1.5 in,\n")
+                output_file.write("    lmargin   = 1.00 in,\n")
+                output_file.write("    rmargin   = 1.00 in,\n")
+                output_file.write("    bmargin   = 1.00 in,\n")
+                output_file.write("    headheight  = 0.50 in,\n")
+                output_file.write("    headsep   = 0.25 in\n")
+                output_file.write("}\n")
+                output_file.write("\n")
+                output_file.write("\\usepackage{fancyhdr}\n")
+                output_file.write("\\pagestyle{fancy}\n")
+                output_file.write("\\fancyhf{}\n")
+                output_file.write("\\fancyhead[L]{Crush Party 2016}\n")
 
-#                 output_file.write("\n");
-#                 output_file.write("\\documentclass[a4paper]{article}\n");
-#                 output_file.write("\n");
-#                 output_file.write("\\usepackage[english]{babel}\n");
-#                 output_file.write("\\usepackage[utf8]{inputenc}\n");
-#                 output_file.write("\\usepackage{parskip}\n");
-#                 output_file.write("\n");
-#                 output_file.write("\\usepackage{geometry}\n");
-#                 output_file.write("\\geometry{\n");
-#                 output_file.write("    tmargin   = 1.5 in,\n");
-#                 output_file.write("    lmargin   = 1.00 in,\n");
-#                 output_file.write("    rmargin   = 1.00 in,\n");
-#                 output_file.write("    bmargin   = 1.00 in,\n");
-#                 output_file.write("    headheight  = 0.50 in,\n");
-#                 output_file.write("    headsep   = 0.25 in\n");
-#                 output_file.write("}\n");
-#                 output_file.write("\n");
-#                 output_file.write("\\usepackage{fancyhdr}\n");
-#                 output_file.write("\\pagestyle{fancy}\n");
-#                 output_file.write("\\fancyhf{}\n");
-#                 output_file.write("\\fancyhead[L]{Crush Party 2016}\n");
+                def print_label (index):
+                    value = person1[index]
+                    if value != emptyID:
+                        output_file.write(", " + constant2string[value])
 
-#                 # output_file.write("\\fancyhead[R]{Results For: " + person.getName() + " \\\\\n");
-#                 # output_file.write("(" + person.getGender() + ", " + person.getYear() +
-#                 #         ", " + person.getCollege() + ", " + person.getMajor() + ")}\n");
+                person1name = constant2string[person1[index_of_["name"]]]
+                output_file.write("\\fancyhead[R]{Results For: ")
+                output_file.write(person1name + " \\\\\n")
+                output_file.write("(" + netID)
+                for index in index_of_["info"]:
+                    if type(index) is int:
+                        print_label(index)
+                    elif type(index) is list:
+                        for subindex in index:
+                            print_label(subindex)
+                output_file.write(")}\n")
 
-#                 output_file.write("\n");
-#                 output_file.write("\\begin{document}\n");
-#                 output_file.write("\n");
-#                 output_file.write("\\section*{Best Results}\n");
-#                 output_file.write("\n");
-#                 writeOneTable(document, person, "List 1", 0);
-#                 writeOneTable(document, person, "List 2", 2);
-#                 writeOneTable(document, person, "List 3", 4);
-#                 output_file.write("\\newpage\n");
-#                 output_file.write("\\section*{Worst Results}\n");
-#                 output_file.write("\n");
-#                 writeOneTable(document, person, "List 1", 1);
-#                 writeOneTable(document, person, "List 2", 3);
-#                 writeOneTable(document, person, "List 3", 5);
-#                 output_file.write("\\end{document}\n");
+                output_file.write("\n")
+                output_file.write("\\begin{document}\n")
+                output_file.write("\n")
 
-#     }
+                for index1 in index_of_["matches1"]:
+                    for index2 in index_of_["matches2"]:
 
-#     private static void writeOneTable (PrintWriter document, Student person,
-#                                   String listName, int index) {
+                        title1 = meaning_of_[tuple([index1])]
+                        title2 = meaning_of_[index2]
 
-#         List<Student> matches = person.getMatchesForList(index);
+                        preposition = ""
+                        if index2 != index_of_overall:
+                            preposition = "by "
 
-#         output_file.write("\\subsection*{" + listName + "}\n");
-#         output_file.write("\n");
-#         output_file.write(person.getDescriptionForList(index) + " \\\\\n");
-#         output_file.write("\n");
-#         output_file.write("\\begin{tabular}{| l | l | l | l | l | l |}\n");
-#         output_file.write("    \\hline\n");
-#         output_file.write("    Name & Match & Gender & Year & College & Major \\\\\n");
-#         output_file.write("    \\hline\n");
-#         for (int i = 0; i < 10; i++) {
-#             Student match = matches.get(i);
-#             String gen;
-#             if (match.getGender() != null && match.getGender() == Gender.NOTHING) {
-#                 gen = "";
-#             } else if (match.getGender() != null) {
-#                 String genA = match.getGender().toString().substring(0, 1);
-#                 String genB = match.getGender().toString().substring(1).toLowerCase();
-#                 gen = genA + genB;
-#             } else {
-#                 gen = "";
-#             }
+                        title = "Top 10 " + title1 + " Matches " + preposition + title2
+                        output_file.write("\\section*{" + title + "}\n")
 
-#             output_file.write("    " + i + ". " + match.getName() + " \n& "
-#                     + Math.round(person.getPercentagesForList(index).get(i) * 100) + "\\% & "
-#                     + gen + " & " + match.getYear() + " & "
-#                     + match.getCollege() + " & " + match.getMajor() + " \\\\");
-#         }
-#         output_file.write("    \\hline\n");
-#         output_file.write("\\end{tabular}\n");
-#         output_file.write("\n");
+                        output_file.write("\n")
+                        output_file.write("\\begin{tabular}{| l | l")
+                        for label in index_of_["info"]:
+                            output_file.write(" | l")
+                        output_file.write(" |}\n")
 
-#     }
+                        output_file.write("    \\hline\n")
+                        output_file.write("    Name & Match \\%")
+                        for label in index_of_["info"]:
+                            output_file.write(" & " + meaning_of_[newtuple(label)])
+                        output_file.write(" \\\\\n")
+                        output_file.write("    \\hline\n")
 
-# }
+                        results = matches[person1id][index1][index2]
 
+                        for pair in results:
+                            person2id = pair[0]
+                            person2 = people[person2id]
+                            score = int(pair[1] / num_questions * 100)
+
+                            person2name = constant2string[person2[index_of_["name"]]]
+                            output_file.write("    " + person2name + " & " + str(score))
+
+                            for index in index_of_["info"]:
+                                if type(index) is int:
+                                    output_file.write(" & " + constant2string[person2[index]])
+                                elif type(index) is list:
+                                    first = True
+                                    for subindex in index:
+                                        word = constant2string[person2[subindex]]
+                                        if len(word) != 0:
+                                            if first:
+                                                first = False
+                                                output_file.write(" & ")
+                                            else:
+                                                output_file.write(", ")
+                                            output_file.write(constant2string[person2[subindex]])
+
+                            output_file.write(" \\\\\n")
+
+                        output_file.write("    \\hline\n")
+                        output_file.write("\\end{tabular}\n")
+                        output_file.write("\n")
+
+                    output_file.write("\\newpage\n\n");
+                
+                output_file.write("\\end{document}\n")
+
+        ### Done! ###
+        
+        print("Done!")
 
 match()
